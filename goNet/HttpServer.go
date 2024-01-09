5111390713f4273dfa5c2e9f187c9ac11b3da5b1
Prateek911 <prateek.work911@gmail.com>
2024-01-09 13:51:11 +0530

    Server creation using http package

---
+++ goNet/HttpServer.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type requestHandler struct{}
+
+func (rq *requestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
+
+func main() {
+	handler := &requestHandler{}
+	server := &http.Server{Addr: ":8080",
+		Handler: handler}
+
+	fmt.Println("Server running on http://localhost:8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Server failed to start:", err)
+	}
+}
